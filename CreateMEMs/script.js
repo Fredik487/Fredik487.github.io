f655de2f4f1fc5445596c7dc38d8eb68ef55db11
Valik <fredik874@gmail.com>
2020-02-20 19:54:45 +0200

    Add resume(lab1)

--- CreateMEMs/script.js
+++ CreateMEMs/script.js
@@ -1,7 +1,12 @@
+var text = document.getElementById('text');
+var canv = document.getElementById("canvas");
+var ctx = canv.getContext("2d");
+
+ctx.fillStyle = "#d3d3d3";
+ctx.font = "20px Arial";
+ctx.fillText("Здесь будет фото",60,100);
+
 document.getElementById('btn').onclick = function(){
-	var text = document.getElementById('text').value;
-	window.canv = document.getElementById("canvas");
-	var ctx = canv.getContext("2d");
 
 	var grd = ctx.createLinearGradient(0,0,500,0);
 	grd.addColorStop(0,"red");
@@ -11,17 +16,17 @@ document.getElementById('btn').onclick = function(){
 	ctx.fillRect(0,0,300,200);
 	ctx.fillStyle = "white";
 	ctx.font = "30px Arial";
-	ctx.fillText(text,20,100);
+	ctx.fillText(text.value,20,100);
 };
 
 function getImg(){
     var link=document.createElement("a");
     link.download="download";
     link.href=canv.toDataURL(["image/png"]);
+    text.value ="";
     link.click();
 }
 
 //выбор размера картинки
 //выбор фона
-//выбор вырвнивание текста
-//
+//выбор выравнивание текста
