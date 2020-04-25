61c4c14f7c0fdec5266a41041624f6c3e7420850
Valik <fredik874@gmail.com>
2020-04-25 17:14:34 +0300

    Astro:add func getH

--- Astro/script.js
+++ Astro/script.js
@@ -34,7 +34,7 @@ function acos(int){
 }
 function toNormTime(time) {
 	//time++;
-	let t = m.round(time)+" h : "+m.round(( time - m.floor(time) )*60)+" min";
+	let t = m.trunc(time)+" h : "+m.trunc(( time - m.floor(time) )*60)+" min";
 	return t;
 }
 	//show(showBlock,DhrTime);
@@ -46,7 +46,7 @@ function b(int){
 	show(textb,bb);
 	bb=rad(bb);
 	console.log(bb);
-
+	return grad(bb); 
 	
 }
 function show(block,msg){
@@ -56,7 +56,7 @@ function addshow(block,msg){
 	block.innerHTML+=msg;
 }
 function Dhf(lat,b){
-	return (24/m.PI)*acos(-tg(lat)*tg(b));
+	return (24/m.PI)*acos(tg(lat)*tg(b));
 }
 function Dhr(lat,b){
 	let r = Dhf(lat,b) + (Dhf(lat,b)/100);
@@ -69,19 +69,24 @@ function Dhr(lat,b){
 
 
 function findDhr(Dh){
+	Dh.replace(',','.');
 	parseInt(Dh);
 	let Dh_lenght=(String(Dh).length - String(Math.trunc(Dh)).length)-1;
-	let k;
-	if(Dh_lenght==1){k=0.1}
-	if(Dh_lenght==2){k=0.01}
-	if(Dh_lenght==3){k=0.001}
-	if(Dh_lenght==4){k=0.0001}
-	if(Dh_lenght==5){k=0.00001}
-	//console.clear();
 	show(findDh,"");
+	let k;
+	
+	if(Dh_lenght==1){k=0.1;	show(findDh,"Точность: "+(111.3*k)+"km\n");}
+	if(Dh_lenght==2){k=0.01;show(findDh,"Точность: "+(111.3*k)+"km\n")}
+	if(Dh_lenght==3){k=0.001;show(findDh,"Точность: "+(111.3*k)+"km\n")}
+	if(Dh_lenght==4){k=0.0001;show(findDh,"Точность: "+(111.3*k).toFixed(5)+"km\n")}
+	if(Dh_lenght==5){k=0.00001;show(findDh,"Точность: "+(111.3*k).toFixed(5)+"km\n")}
+	if(Dh_lenght==-1){k=1;Dh_lenght=0;show(findDh,"Точность: "+(111.3*k)+"km\n");}
+	console.clear();
+
 	for (var i = -90; i<=90; i+=k) {
-		let v = NDhr(rad(i),bb);
-		if(v.toFixed(Dh_lenght)==Dh){
+		let v = NDhr(rad(i),rad(bb));
+		if(isNaN(v)){continue;}
+		if(v.toFixed(Dh_lenght)==(Dh-0.0279)){//поправка,так как формула считает центр солнца,из за чего возникате разница в//0.0278мин с програмой на телефоне
 			let textDh ="Широта:"+ i.toFixed(Dh_lenght)+"<br>";
 			findDh.innerHTML+=textDh;
 			//show(findDh,textDh);
@@ -91,7 +96,7 @@ function findDhr(Dh){
 }
 
 function findDhr2(Dh,st,end,k){
-	parseInt(Dh);
+	parseFloat(Dh);
 	//console.clear();
 	for (var i = st; i<=end+1; i+=k) {
 		let v = NDhr(rad(i),rad(b));
@@ -111,12 +116,14 @@ function NDhr(lat,b){
 	return NDhf(lat,b) + (NDhf(lat,b)/100);
 }
 function NDhr2(lat){
-	lat=grad(lat);
+	lat.replace(',','.');
 	let lat_lenght=(String(lat).length - String(Math.trunc(lat)).length)-1;
+	if(lat_lenght==-1){lat_lenght=0;};
 	lat=rad(lat);
-	let Dhr=NDhf(lat,bb) + (NDhf(lat,bb)/100);
+	let Dhr=NDhf(lat,rad(bb)) + (NDhf(lat,rad(bb))/100);
 	console.log(Dhr.toFixed(lat_lenght))
-	let DhrTime = toNormTime(Dhr);
+	let DhrTime = toNormTime(Dhr+0.0278);
+	Dhr+=0.0278;//поправка,так как формула считает центр солнца,из за чего возникате разница в//0.0278мин с програмой на телефоне 
 	show(showBlock,DhrTime+"<br>"+Dhr.toFixed(lat_lenght));
 	
 }
@@ -200,13 +207,48 @@ function getH3(Lat,b,H,sh){
 function findH2(hReal,H){
 	findh.innerHTML="";
 	let hReal_lenght=(String(hReal).length - String(Math.trunc(hReal)).length)-1;
-	for (var i = -90; i<=90; i+=0.1) {
+	let k;
+	if(hReal_lenght==1){k=0.1}
+	if(hReal_lenght==2){k=0.01}
+	if(hReal_lenght==3){k=0.001}
+	if(hReal_lenght==4){k=0.0001}
+	if(hReal_lenght==5){k=0.00001}
+	for (var i = 0; i<=90; i+=k) {
 		let h = getH3(i,bb,0,false);
 		console.log(hReal_lenght,h.toFixed(hReal_lenght),grad(hReal).toFixed(hReal_lenght));
 		if(h.toFixed(hReal_lenght) == parseFloat(hReal).toFixed(hReal_lenght)){
 			let texth ="Широта:"+ i.toFixed(hReal_lenght)+"<br>";
 			findh.innerHTML+=texth;
-			console.log("hReal",grad(hReal).toFixed(2),"Lat2:",i.toFixed(2),"h2:",h.toFixed(1));
+			console.log("hReal",grad(hReal).toFixed(hReal_lenght),"Lat2:",i.toFixed(hReal_lenght),"h2:",h.toFixed(hReal_lenght));
 		}
 	}
+}
+
+function DayAndMonth(el) {
+	let value = el.value;
+	let month_lenght=(String(value).length - String(Math.trunc(value)).length)-1;
+	let k;
+
+	if(value==''){return};
+	if(month_lenght==1){k=10;}else{k=100;}
+	var date = {day:Math.trunc(value),month:Math.round((value - Math.trunc(value))*k)}
+	console.log("Date",date.day,date.month,"month_lenght:",month_lenght);
+	if( (date.day==0) || (date.month==0) ){show(textb,"Что-то не так(12.02)");return}
+
+	bb = findB(CountDayOfYear(date.day,date.month),1).toFixed(4);
+	bb = b(bb);
+}
+
+
+function t(h,a){
+    let t = ( m.cos(rad(h))* -m.sin(rad(a)) )/m.sin(rad(bb));
+    return grad(m.abs(t));
+}
+function getH(time){
+    let H = (time - 12)*15;
+    return H;
+}
+function getTime(H){
+    let time = (H/15)+12;
+    return time;
 }
